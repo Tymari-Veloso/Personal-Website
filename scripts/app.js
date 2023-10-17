5ffc00d9208752bfc297d837b5c798191dfc0862
Tymari Veloso <tymariveloso@gmail.com>
2023-10-17 00:00:04 -0700

    Added background color change function

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,14 @@
-console.log('Hello World!')
+let colorBtn = document.querySelector("#color-btn");
+
+colorBtn.addEventListener("click", myFunction);
+function myFunction(){
+    if(document.body.style.backgroundColor == "black"){
+        document.body.style.backgroundColor = "white"
+        console.log("Changing Background to White")
+    }
+    else{
+        document.body.style.backgroundColor = "black"
+        console.log("Changing Background to Black")
+    }
+}
+
