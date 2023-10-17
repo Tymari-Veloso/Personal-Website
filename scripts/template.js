59aea66c2572a675198583065bbc025a27bdd8f2
Tymari Veloso <tymariveloso@gmail.com>
2023-10-16 22:51:46 -0700

    Added script to template header and footer

---
+++ scripts/template.js
@@ -0,0 +1,31 @@
+class MyHeader extends HTMLElement{ //Custom element that contains header html content
+    connectedCallback(){    //Run this code when it is connected to a document
+        this.innerHTML = `
+        <header class="header">
+            <h1 class="title">Tymari Veloso</h1>
+            <nav class="nav-bar">
+                <ul class="nav-list">
+                    <li><a href="pages/about.html">About</a></li>
+                    <li><a href="pages/projects.html">Projects</a></li>
+                    <li><a href="pages/contact.html">Contact</a></li>
+                </ul>
+            </nav>
+        </header>
+    `
+    }
+}
+class MyFooter extends HTMLElement{ //Custom element that contains footer html content
+    connectedCallback(){    //Run this code when it is connected to a document
+        this.innerHTML = `
+        <footer class="footer">
+            <div class="contact-links">
+                <a href="https://github.com/Tymari-Veloso"><img src="src/logos/github-mark-white.png" alt="GitHub logo link"></a>
+                <a href="https://www.linkedin.com/in/tymari-veloso/"><img src="src/logos/LI-In-Bug.png" alt="Linkedin logo link"></a>
+            </div>
+        </footer>
+    `
+    }
+}
+//Defines the custom elements above so they can be used insde my html document
+customElements.define("my-header", MyHeader)
+customElements.define("my-footer", MyFooter)
